e39bd89dd6a8de2a72bf07ed26b59c8e73bb1cd7
kwhipke1 <kwhipke1@gmail.com>
2017-12-28 15:48:58 -0800

    add config to support pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup
 
 setup(name='fantasy_football_auction',
+      packages=['fantasy_football_auction'],
       version='0.9.1',
+      description='Python library simulating a fantasy football auction. Intended to be used for AI, but you should be '
+                  'able to use this for other purposes as well. This task assumes that each draftable player has a '
+                  'specific value (for example, looking at the ratings from FantasyPros).',
+      author='Kyle Hipke',
+      author_email='kwhipke1@gmail.com',
+      url='https://github.com/chairbender/fantasy-football-auction',
+      download_url='https://github.com/chairbender/fantasy-football-auction/archive/0.9.1.tar.gz',
+      keywords=['AI', 'football', 'fantasy', 'auction'],
+      classifiers=[],
       python_requires=">=3.6",
       install_requires=[]
       )
