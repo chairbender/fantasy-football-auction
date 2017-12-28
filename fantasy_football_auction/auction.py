4e22fc6dcf186bf2df606d599c57b33a79909ff3
kwhipke1 <kwhipke1@gmail.com>
2017-12-27 16:53:59 -0800

    refactor Owner, add more tests for it, and fix bugs

--- fantasy_football_auction/auction.py
+++ fantasy_football_auction/auction.py
@@ -58,15 +58,43 @@ class Purchase:
         self.roster_slot = roster_slot
 
 
+class OwnerRosterSlot(RosterSlot):
+    """
+    Just like a RosterSlot, but has the possibility of having a player occupying or not occupying it.
+
+    Attributes:
+        :ivar Player occupant: player which has filled this slot. None if not filled.
+    """
+    @classmethod
+    def from_roster_slot(cls, roster_slot):
+        """
+
+        :param RosterSlot roster_slot: to create from
+        :return OwnerRosterSlot: an OwnerRosterSlot with the same state as roster_slot
+        """
+        return cls(roster_slot.positions, roster_slot.abbreviation)
+
+    def __init__(self, positions, abbreviation):
+        """
+
+        :param list(Position) positions: set of positions that can occupy this slot
+        :param str abbreviation: abbreviation to use to refer to this roster slot
+        """
+        super(OwnerRosterSlot, self).__init__(positions, abbreviation)
+        self.occupant = None
+
+
+
 class Owner:
     """
     Represents an owner during an auction
 
     Attributes:
-        :ivar int money : amount of money the owner has remaining
-        :ivar list(RosterSlot) roster: slots this player has left to fill
-        :ivar list(Purchase) purchases: purchases this owner has made
-        :ivar int id: id of this owner.
+        :ivar int money : amount of money the owner has remaining. Read only.
+        :ivar list(OwnerRosterSlot) roster: the Owner's current roster, indicating which positions need to be filled
+            (where occupant is None) and which have been filled (where occupant indicates the player that filled it).
+            This will be ordered by most -> least specific slots (so the bench slots will always be last). Read only.
+        :ivar int id: id of this owner. Read only.
     """
 
     def __init__(self, money, roster, owner_id):
@@ -77,12 +105,38 @@ class Owner:
         :param int owner_id: unique id of the owner to distinguish it from other owners, Must be [0,num owners)
         """
         self.money = money
-        # create our own copy so we can sort by the number of accepted positions
-        self.roster = list(roster)
+        self.roster = [OwnerRosterSlot.from_roster_slot(roster_slot) for roster_slot in roster]
         self.roster.sort(key=lambda roster_slot: roster_slot.num_accepted())
-        # tracks the purchases made by this owner
-        self.purchases = []
         self.id = owner_id
+
+    def _slot_in(self, player):
+        """
+
+        :param Player player: player to slot into the owner's current roster. Puts this
+            player in the most specific slot that it can occupy, kicking out any lower-value players
+            (and finding alternate slots for them) if the slot is occupied. If this player is lower valued than
+            that of a player in an occupied slot, finds the next most specific slot and so on.
+
+            This has the side effect of ensuring that the highest value players are on the start rather than the
+            bench. It also seems to match how fantasy owners like to order their team.
+
+        """
+
+        # find the most specific slot this player can fit into where they are the highest value.
+        for roster_slot in self.roster:
+            if roster_slot.accepts(player):
+                # check if the slot is already occupied
+                if roster_slot.occupant is None:
+                    # We're done. Just put the new person here.
+                    roster_slot.occupant = player
+                    break
+                elif player.value > roster_slot.occupant.value:
+                    # our value is greater, so replace
+                    replacee = roster_slot.occupant
+                    roster_slot.occupant = player
+                    self._slot_in(replacee)
+                    break
+                # our value was less, so keep looking
 
     def buy(self, player, cost):
         """
@@ -97,19 +151,21 @@ class Owner:
         """
         if cost > self.max_bid():
             raise InsufficientFundsError()
-        elif not any(roster_slot.accepts(player) for roster_slot in self.roster):
+        elif not any(roster_slot.accepts(player) and roster_slot.occupant is None for roster_slot in self.roster):
             raise NoValidRosterSlotError()
-        elif any(purchase.player == player for purchase in self.purchases):
+        elif self._is_owned(player):
             raise AlreadyPurchasedError()
 
         self.money -= cost
-        # remove the roster slot that is the most specific
-        # we know they are sorted by specificity
-        for roster_slot in self.roster:
-            if roster_slot.accepts(player):
-                self.purchases.append(Purchase(player, cost, roster_slot))
-                self.roster.remove(roster_slot)
-                break
+        self._slot_in(player)
+
+    def _is_owned(self, player):
+        """
+
+        :param Player player: player to check
+        :return boolean: true iff this player is already owned by this owner
+        """
+        return any(roster_slot.occupant == player for roster_slot in self.roster)
 
     def max_bid(self):
         """
@@ -118,7 +174,7 @@ class Owner:
         one dollar per slot and must fill all slots).
 
         """
-        return (self.money + 1) - len(self.roster)
+        return (self.money + 1) - self.remaining_picks()
 
     def can_buy(self, player, bid):
         """
@@ -126,10 +182,12 @@ class Owner:
         :param Player player: player to check
         :param int bid: bid amount
         :return boolean: true iff this owner has space in the roster for the given player and has enough to
-            bid the given amount
+            bid the given amount and has not already purchased this player
         """
 
-        return any(roster_slot.accepts(player) for roster_slot in self.roster) and bid <= self.max_bid()
+        return any(roster_slot.accepts(player) for roster_slot in self.roster) and \
+            bid <= self.max_bid() and \
+            not self._is_owned(player)
 
     def remaining_picks(self):
         """
@@ -137,7 +195,7 @@ class Owner:
         :return int: the number of picks left for this owner to make until their roster is filled, 0 if full
         """
 
-        return len(self.roster)
+        return len([None for roster_slot in self.roster if roster_slot.occupant is None])
 
     def possible_nominees(self, players):
         """
@@ -146,7 +204,7 @@ class Owner:
         :return list(Player): a list of players that could be legally nominated by this owner
         """
 
-        return [player for player in players if self.can_buy(player,1)]
+        return [player for player in players if self.can_buy(player, 1)]
 
     def start_value(self):
         """
@@ -155,8 +213,8 @@ class Owner:
             starting lineup
         """
 
-        return reduce(lambda x, y: x + y, [purchase.player.value for purchase in self.purchases
-                                           if purchase.roster_slot != RosterSlot.BN])
+        return reduce(lambda x, y: x + y, [roster_slot.occupant.value for roster_slot in self.roster
+                                           if roster_slot != RosterSlot.BN])
 
     def bench_value(self):
         """
@@ -165,8 +223,8 @@ class Owner:
             bench lineup
         """
 
-        return reduce(lambda x, y: x + y, [purchase.player.value for purchase in self.purchases
-                                           if purchase.roster_slot == RosterSlot.BN])
+        return reduce(lambda x, y: x + y, [roster_slot.occupant.value for roster_slot in self.roster
+                                           if roster_slot == RosterSlot.BN])
 
     def score(self, starter_value):
         """
@@ -180,7 +238,7 @@ class Owner:
             value of their players and the ratio of starter to bench value
         """
 
-        return self.start_value() * starter_value - self.bench_value() * (1 - starter_value)
+        return self.start_value() * starter_value + self.bench_value() * (1 - starter_value)
 
 
 class Auction:
--- fantasy_football_auction/auction_test.py
+++ fantasy_football_auction/auction_test.py
@@ -6,12 +6,13 @@ from fantasy_football_auction.player import Player
 from fantasy_football_auction.position import RosterSlot, Position
 
 
-class OwnerSlotTestCase(TestCase):
+class OwnerTestCase(TestCase):
 
     def test_buy_error(self):
         owner = Owner(20, [RosterSlot.QB, RosterSlot.QB, RosterSlot.TE], 0)
         player_qb = Player("blahQB", Position.QB, 1)
         player_te = Player("blahTE", Position.TE, 1)
+        player_te2 = Player("blahTE2", Position.TE, 2)
         player_wr = Player("blahWR", Position.WR, 1)
 
         with self.assertRaises(InsufficientFundsError):
@@ -22,7 +23,7 @@ class OwnerSlotTestCase(TestCase):
 
         with self.assertRaises(NoValidRosterSlotError):
             owner.buy(player_te, 3)
-            owner.buy(player_te, 3)
+            owner.buy(player_te2, 3)
 
         with self.assertRaises(AlreadyPurchasedError):
             owner.buy(player_qb, 3)
@@ -35,26 +36,99 @@ class OwnerSlotTestCase(TestCase):
             owner.buy(player_te, 11)
 
     def test_buy(self):
+        owner = Owner(20, [RosterSlot.WR, RosterSlot.WRRBTE, RosterSlot.BN], 0)
+        player_wr = Player("blahWR", Position.WR, 1)
+        player_wr2 = Player("blahWR2", Position.WR, 2)
+        player_te = Player("blahTE", Position.TE, 3)
+
+        owner.buy(player_wr, 5)
+
+        self.assertEqual(15, owner.money)
+        self.assertEqual(player_wr, owner.roster[0].occupant)
+
+        owner.buy(player_wr2, 5)
+        # higher value WR should replace the existing qb
+        self.assertEqual(player_wr2, owner.roster[0].occupant)
+        self.assertEqual(player_wr, owner.roster[1].occupant)
+        self.assertEqual(10, owner.money)
+
+        owner.buy(player_te, 5)
+        self.assertEqual(5, owner.money)
+        # this should displace the WRRBTE slot, leaving the low-value wr in the bench slot
+        self.assertEqual(player_wr2, owner.roster[0].occupant)
+        self.assertEqual(player_te, owner.roster[1].occupant)
+        self.assertEqual(player_wr, owner.roster[2].occupant)
+        self.assertEqual(5, owner.money)
+
+        # we should still get the same outcome even if we do it in a different order
+        owner = Owner(20, [RosterSlot.WR, RosterSlot.WRRBTE, RosterSlot.BN], 0)
+        owner.buy(player_te, 5)
+        owner.buy(player_wr2, 5)
+        owner.buy(player_wr, 5)
+        self.assertEqual(player_wr2, owner.roster[0].occupant)
+        self.assertEqual(player_te, owner.roster[1].occupant)
+        self.assertEqual(player_wr, owner.roster[2].occupant)
+        self.assertEqual(5, owner.money)
+
+    def test_can_buy(self):
+        owner = Owner(20, [RosterSlot.QB, RosterSlot.QB, RosterSlot.TE], 0)
+        player_qb = Player("blahQB", Position.QB, 1)
+        player_te = Player("blahTE", Position.TE, 1)
+        player_wr = Player("blahWR", Position.WR, 1)
+
+        self.assertFalse(owner.can_buy(player_qb, 21))
+
+        self.assertFalse(owner.can_buy(player_wr, 3))
+
+        owner.buy(player_te, 3)
+        self.assertFalse(owner.can_buy(player_te, 3))
+
+        owner.buy(player_qb, 3)
+        playerQBDupe = Player("blahQB", Position.QB, 1)
+        self.assertFalse(owner.can_buy(playerQBDupe, 1))
+
+        owner = Owner(20, [RosterSlot.QB, RosterSlot.TE], 0)
+        owner.buy(player_qb, 10)
+        self.assertFalse(owner.can_buy(player_te, 11))
+
+    def test_max_bid(self):
         owner = Owner(20, [RosterSlot.QB, RosterSlot.QB, RosterSlot.TE], 0)
         player_qb = Player("blahQB", Position.QB, 1)
         player_qb2 = Player("blahQB2", Position.QB, 1)
-        player_te = Player("blahTE", Position.TE, 1)
+
+        self.assertEqual(18, owner.max_bid())
 
         owner.buy(player_qb, 5)
+        self.assertEqual(14, owner.max_bid())
 
-        self.assertEqual(15, owner.money)
-        purchase_qb = owner.purchases[0]
-        self.assertEqual(player_qb, purchase_qb.player)
-        self.assertEqual(5, purchase_qb.cost)
-        self.assertEqual(RosterSlot.QB, purchase_qb.roster_slot)
-
-        self.assertTrue(RosterSlot.QB in owner.roster)
         owner.buy(player_qb2, 5)
-        self.assertTrue(RosterSlot.QB not in owner.roster)
-        self.assertEqual(10, owner.money)
+        self.assertEqual(10, owner.max_bid())
 
-        self.assertTrue(RosterSlot.TE in owner.roster)
+    def test_scoring_value(self):
+        owner = Owner(20, [RosterSlot.WR, RosterSlot.WRRBTE, RosterSlot.BN], 0)
+        player_wr = Player("blahWR", Position.WR, 1)
+        player_wr2 = Player("blahWR2", Position.WR, 2)
+        player_te = Player("blahTE", Position.TE, 3)
         owner.buy(player_te, 5)
-        self.assertTrue(RosterSlot.TE not in owner.roster)
-        self.assertEqual(5, owner.money)
+        owner.buy(player_wr2, 5)
+        owner.buy(player_wr, 5)
+
+        self.assertAlmostEqual(5, owner.start_value())
+        self.assertAlmostEqual(1, owner.bench_value())
+        self.assertAlmostEqual(3, owner.score(.5))
+
+        owner = Owner(20, [RosterSlot.WR, RosterSlot.WRRBTE, RosterSlot.BN], 0)
+        player_wr = Player("blahWR", Position.WR, 5)
+        player_wr2 = Player("blahWR2", Position.WR, 10)
+        player_te = Player("blahTE", Position.TE, 20)
+        owner.buy(player_te, 5)
+        owner.buy(player_wr2, 5)
+        owner.buy(player_wr, 5)
+
+        self.assertAlmostEqual(30, owner.start_value())
+        self.assertAlmostEqual(5, owner.bench_value())
+        self.assertAlmostEqual(17.5, owner.score(.5))
+
+
+
 
