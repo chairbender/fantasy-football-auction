d6081450527e3d7aafffacc63f38188f43c8e7c9
kwhipke1 <kwhipke1@gmail.com>
2017-12-28 15:25:48 -0800

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='fantasy_football_auction',
-      version='0.5.1',
+      version='0.9.1',
       python_requires=">=3.6",
       install_requires=[]
       )
